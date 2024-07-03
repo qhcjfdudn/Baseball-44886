7b94c52a2d2f6a12007d1521c0262c863f26b892
hack.jang <hack.jang@samsung.com>
2024-07-03 14:55:07 +0900

    feature 30p

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -7,3 +7,7 @@ TEST(Baseball, ThrowExceptionWhenInputLengthIsUnmached) {
 	Baseball game;
 	EXPECT_THROW(game.guess(string("12")), length_error);
 }
+TEST(Baseball, ThrowExceptionWhenInvalidChar) {
+	Baseball game;
+	EXPECT_THROW(game.guess(string("12s")), invalid_argument);
+}
